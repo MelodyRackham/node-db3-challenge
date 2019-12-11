13b6997e0d19ad41531385fcd6837458398acae6
Mel Maki <melody.rackham@gmail.com>
2019-12-11 15:01:16 -0700

    Finished SQL Queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,30 @@
 -- Multi-Table Query Practice
 
--- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+-- ✅  Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select productname, categoryname
+from product
+join category
+on categoryid = category.id
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+--  ✅  Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select o.id, s.companyname
+from [order] as o
+join shipper as s
+where o.orderdate < '2012-08-09'
+and o.Shipvia = s.id
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+--  ✅ Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select p.productname, p.quantityperunit, e.firstname
+from product as p
+ join orderdetail as od on od.productid = p.id
+ join [order] as o on  od.orderid = o.id
+ join employee as e on o.employeeid = e.id
+where o.id = '10251'
+
+-- ✅ Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select o.id, c.companyname, e.lastname
+from [order] as o
+join customer as c on o.customerid = c.id
+join employee as e on o.employeeid = e.id
